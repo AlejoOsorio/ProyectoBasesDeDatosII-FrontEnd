b0304851b074b946241b2b65998ba5c90a53af97
AlejoOsorio <112147774+AlejoOsorio@users.noreply.github.com>
2024-11-15 11:54:12 -0500

    se agregaron comoponentes
    - ver-pedido
    - inicio
    - login
    - crear-pedido

---
+++ src/app/componentes/crear-pedido/crear-pedido.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-pedido',
+  standalone: true,
+  imports: [],
+  templateUrl: './crear-pedido.component.html',
+  styleUrl: './crear-pedido.component.css'
+})
+export class CrearPedidoComponent {
+
+}
