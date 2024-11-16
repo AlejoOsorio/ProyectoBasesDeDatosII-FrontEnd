d8092df5cb20ce92bffa64295873ee92f4ab6228
StivenF2711 <141967258+StivenF2711@users.noreply.github.com>
2024-11-16 01:39:24 -0500

    Vistas creadas

---
+++ src/app/componentes/eliminar-pedido/eliminar-pedido.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eliminar-pedido',
+  standalone: true,
+  imports: [],
+  templateUrl: './eliminar-pedido.component.html',
+  styleUrl: './eliminar-pedido.component.css'
+})
+export class EliminarPedidoComponent {
+
+}
